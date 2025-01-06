9942bb4101694cb879ab52e2f789eeb82a6af5f5
andrianfaa <andrianfadhilla@gmail.com>
2025-01-06 23:08:40 +0700

    feat: mongodb docker compose

--- backend/src/database/mongodb.ts
+++ backend/src/database/mongodb.ts
@@ -5,7 +5,7 @@ mongoose.plugin(schemaNormalizer());
 
 export default async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!).then(() => console.log("✅ MongoDB connected!"));
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING!).then(() => console.log("✅ MongoDB connected!"));
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
